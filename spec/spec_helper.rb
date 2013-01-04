3085ac6b82f11d6068950ab779cca774fd215cce
Frank C. Eckert <frank.eckert@boost-project.com>
2013-01-04 10:36:48 +0100

    exclude specs from code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
 require 'iban-tools'
 
